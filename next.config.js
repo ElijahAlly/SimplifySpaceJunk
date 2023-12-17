3c90d9f0de8355a78808fc2a76352a7038c29ced
MusaByte <75961076+ElijahAlly@users.noreply.github.com>
2023-12-17 18:09:12 -0500

    reverting nextjs to latest to deploy with vercel

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
+// const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
   // Use the prefix for GitHub Pages
-  basePath: isProd ? '/SimplifySpaceJunk' : '',
-  assetPrefix: isProd ? '/SimplifySpaceJunk/' : '',
-  output: 'standalone',
+  // basePath: isProd ? '/SimplifySpaceJunk' : '',
+  // assetPrefix: isProd ? '/SimplifySpaceJunk/' : '',
 }
 
 module.exports = nextConfig
